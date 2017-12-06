ec1a997ed9c6bc265a98eb0f4e970db876b27f95
Admin <vitaliy.kazakoff2013@gmail.com>
2017-12-06 12:33:09 +0200

    Ferst Load project

---
+++ task19JSON/task19JSON/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  task19JSON
+//
+//  Created by Admin on 06.12.2017.
+//  Copyright © 2017 Bioprom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
