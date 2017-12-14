7e59e01785ac4acb895f311a725dd2ff9165c4c2
Admin <vitaliy.kazakoff2013@gmail.com>
2017-12-14 11:00:35 +0200

    program generation UIbutton

--- task19JSON/task19JSON/Controller/QuestionViewController.swift
+++ task19JSON/task19JSON/Controller/QuestionViewController.swift
@@ -13,10 +13,7 @@ class QuestionViewController: UIViewController {
     @IBOutlet private weak var ibLableCategory: UILabel!
     @IBOutlet private weak var ibLableQiestion: UILabel!
     @IBOutlet private weak var ibStackViewVertical: UIStackView!
-    @IBOutlet private weak var ibButtonOption1: UIButton!
-    @IBOutlet private weak var ibButtonOption2: UIButton!
-    @IBOutlet private weak var ibButtonOption3: UIButton!
-    @IBOutlet private weak var ibButtonOption4: UIButton!
+    private var buttonOptions: [UIButton] = []
     
     var question: Question?
     
@@ -28,14 +25,22 @@ class QuestionViewController: UIViewController {
     }
     
     private func setupButton() {
-        guard let option1 = question?.options[0] else { return }
-        ibButtonOption1.setTitle("A. \"" + option1 + "\"", for: .normal)
-        guard let option2 = question?.options[1] else { return }
-        ibButtonOption2.setTitle("B. \"" + option2 + "\"", for: .normal)
-        guard let option3 = question?.options[2] else { return }
-        ibButtonOption3.setTitle("C. \"" + option3 + "\"", for: .normal)
-        guard let option4 = question?.options[3] else { return }
-        ibButtonOption4.setTitle("D. \"" + option4 + "\"", for: .normal)
+        for (index, option) in (question?.options)!.enumerated() {
+            buttonOptions.append(UIButton())
+            buttonOptions[index].titleLabel?.textColor = UIColor.black
+            buttonOptions[index].titleLabel?.isEnabled = true
+            buttonOptions[index].backgroundColor = UIColor.white
+            buttonOptions[index].setTitle("\(index). \"" + option + "\"", for: UIControlState.normal)
+            ibStackViewVertical.addArrangedSubview(buttonOptions[index])
+        }
+//        guard let option1 = question?.options[0] else { return }
+//        ibButtonOption1.setTitle("A. \"" + option1 + "\"", for: .normal)
+//        guard let option2 = question?.options[1] else { return }
+//        ibButtonOption2.setTitle("B. \"" + option2 + "\"", for: .normal)
+//        guard let option3 = question?.options[2] else { return }
+//        ibButtonOption3.setTitle("C. \"" + option3 + "\"", for: .normal)
+//        guard let option4 = question?.options[3] else { return }
+//        ibButtonOption4.setTitle("D. \"" + option4 + "\"", for: .normal)
     }
     
     private func alertShow(title: String, text: String, currectAnswer: Bool) {
